82f7720298b464cb24d424a49a0b6f5fa561c200
mobinhassani9374 <mobinhassani9374@gmail.com>
2020-12-05 15:01:01 +0330

    add usermanage pages

---
+++ Shop.Mvc/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Mvc.Areas.Admin.Controllers
+{
+    public class UserManagementController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        public IActionResult Permision()
+        {
+            return View();
+        }
+    }
+}
