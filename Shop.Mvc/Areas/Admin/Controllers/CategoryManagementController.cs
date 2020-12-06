ab005c44dce6f6c96e0fe601b1edbe5e8168f224
mobinhassani9374 <mobinhassani9374@gmail.com>
2020-12-06 23:58:47 +0330

    get in category

--- Shop.Mvc/Areas/Admin/Controllers/CategoryManagementController.cs
+++ Shop.Mvc/Areas/Admin/Controllers/CategoryManagementController.cs
@@ -12,5 +12,42 @@ namespace Shop.Mvc.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult Get()
+        {
+            var data = new List<Models.CategoryManagement.CategoryViewModel>();
+            data.Add(new Models.CategoryManagement.CategoryViewModel
+            {
+                Id = 1,
+                Text = "سیستم های حفاظتی و ایمنی",
+                Children = new List<Models.CategoryManagement.CategoryViewModel>()
+                   {
+                    new Models.CategoryManagement.CategoryViewModel
+                    {
+                        Id=2,
+                         Text="دزدگیر اماکن",
+                          Children=new List<Models.CategoryManagement.CategoryViewModel>
+                          {
+                          new Models.CategoryManagement.CategoryViewModel
+                          {
+                              Id=5,
+                               Text="پکیچ های آماده نصب"
+                          },
+                          new Models.CategoryManagement.CategoryViewModel
+                          {
+                           Text="دستگاه های دزدگیر مرکزی",
+                            Id=6
+                          },
+                          new Models.CategoryManagement.CategoryViewModel
+                          {
+                           Id=7,
+                            Text="سنسورها"
+                          }
+                          }
+                    }
+                   }
+            });
+            return Json(data);
+        }
     }
 }
