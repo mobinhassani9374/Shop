2a71247d803c93dc2a38b74139410edaaff3744a
mobinhassani9374 <mobinhassani9374@gmail.com>
2020-12-13 11:07:14 +0330

    develop of required and MaxLength Attribute

--- Shop.Domain/Dto/Product/CreateProductDto.cs
+++ Shop.Domain/Dto/Product/CreateProductDto.cs
@@ -9,9 +9,11 @@ namespace Shop.Domain.Dto.Product
     public class CreateProductDto
     {
         [Required(ErrorMessage = "عنوان محصول نمی تواند فاقد مقدار باشد")]
+        [MaxLength(Constants.Product_Title_Length, ErrorMessage = "تعداد کاراکترهای عنوان نمی تواند بیش از @ کاراکتر باشد")]
         public string Title { get; set; }
 
         [Required(ErrorMessage = "توضیحات محصول نمی تواند فاقد مقدار باشد")]
+        [MaxLength(Constants.Product_Description_Length, ErrorMessage = "تعداد کاراکترهای توضیحات نمی تواند بیش از @ کاراکتر باشد")]
         public string Description { get; set; }
 
         [Required(ErrorMessage = "عکس محصول نمی تواند فاقد مقدار باشد")]
--- Shop.Services/Validations/ProductValidation.cs
+++ Shop.Services/Validations/ProductValidation.cs
@@ -7,6 +7,8 @@ using Shop.Utility.Extensions;
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
+using DNTPersianUtils.Core;
+using System.Reflection;
 
 namespace Shop.Services.Validations
 {
@@ -14,35 +16,69 @@ namespace Shop.Services.Validations
     {
         public static ServiceResult IsValid<T>(this T dto)
         {
-            var seriveResult = new ServiceResult(true);
+            var serviceResult = new ServiceResult(true);
 
             var properties = dto.GetType().GetProperties().ToList();
 
             foreach (var property in properties)
             {
                 var customAttributes = property.CustomAttributes.ToList();
+
                 foreach (var attribute in customAttributes)
                 {
-                    if (attribute.AttributeType == typeof(RequiredAttribute))
-                    {
-                        if (property.PropertyType == typeof(String))
-                        {
-                            var value = property.GetValue(dto);
-                            if (string.IsNullOrEmpty(value.ToString()))
-                                seriveResult.AddError(attribute.NamedArguments.Select(c => c.TypedValue.Value.ToString()).FirstOrDefault());
-                        }
+                    var value = property.GetValue(dto);
 
-                        if (property.PropertyType == typeof(IFormFile))
-                        {
-                            var value = property.GetValue(dto);
-                            if(value==null)
-                                seriveResult.AddError(attribute.NamedArguments.Select(c => c.TypedValue.Value.ToString()).FirstOrDefault());
-                        }
-                    }
+                    if (attribute.AttributeType == typeof(RequiredAttribute))
+                        RequiredAttribute(serviceResult, value, property, attribute);
+
+                    if (attribute.AttributeType == typeof(MaxLengthAttribute))
+                        MaxLengthAttribute(serviceResult, value, property, attribute);
                 }
             }
 
-            return seriveResult;
+            return serviceResult;
+        }
+
+        private static ServiceResult RequiredAttribute(ServiceResult serviceResult, object value, PropertyInfo property, CustomAttributeData attribute)
+        {
+            if (property.PropertyType == typeof(String))
+            {
+                if (string.IsNullOrEmpty(value.ToString()))
+                    serviceResult.AddError(attribute.NamedArguments.Select(c => c.TypedValue.Value.ToString()).FirstOrDefault());
+            }
+
+            if (property.PropertyType == typeof(IFormFile))
+            {
+                if (value == null)
+                    serviceResult.AddError(attribute.NamedArguments.Select(c => c.TypedValue.Value.ToString()).FirstOrDefault());
+            }
+
+            return serviceResult;
+        }
+
+        private static ServiceResult MaxLengthAttribute(ServiceResult serviceResult, object value, PropertyInfo property, CustomAttributeData attribute)
+        {
+            int length = (int)attribute.ConstructorArguments.Select(c => c.Value).FirstOrDefault();
+            var errorMessage = "";
+
+            if (property.PropertyType == typeof(String))
+            {
+                if (value.ToString().Length > length)
+                    errorMessage = attribute.NamedArguments.Select(c => c.TypedValue.Value.ToString()).FirstOrDefault();
+            }
+
+            if (property.PropertyType == typeof(IFormFile))
+            {
+                if (((IFormFile)value).Length > length)
+                    errorMessage = attribute.NamedArguments.Select(c => c.TypedValue.Value.ToString()).FirstOrDefault();
+            }
+
+            errorMessage = errorMessage.Replace("@", length.ToPersianNumbers());
+
+            if (!string.IsNullOrEmpty(errorMessage))
+                serviceResult.AddError(errorMessage);
+
+            return serviceResult;
         }
     }
 }
