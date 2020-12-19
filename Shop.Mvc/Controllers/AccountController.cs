af5643e3a0653054c68e8807b9ed77300b463afc
mobinhassani9374 <mobinhassani9374@gmail.com>
2020-12-19 19:32:33 +0330

    add register view

--- Shop.Mvc/Controllers/AccountController.cs
+++ Shop.Mvc/Controllers/AccountController.cs
@@ -69,6 +69,11 @@ namespace Shop.Mvc.Controllers
             return View(model);
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         /// sync
         public IActionResult Sync()
         {
---
+++ Shop.Mvc/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Mvc.Models.Account
+{
+    public class RegisterViewModel
+    {
+        public string PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPasswprd { get; set; }
+    }
+}
