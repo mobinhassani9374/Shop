36e0249ded8ff85dae6cb9f6d412f6633a71a4e2
mobinhassani9374 <mobinhassani9374@gmail.com>
2020-12-20 20:11:35 +0330

    develop of layout

---
+++ Shop.Utility/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Shop.Utility.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string GetFullName(this ClaimsPrincipal claimsPrincipal)
+        {
+            var claim = claimsPrincipal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.GivenName);
+            return claim?.Value;
+        }
+    }
+}
