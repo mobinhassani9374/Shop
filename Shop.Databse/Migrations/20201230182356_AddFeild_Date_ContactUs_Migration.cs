245f7f59a57aab388eb8f6fd746e9457fc75c9a2
mobinhassani9374 <mobinhassani9374@gmail.com>
2020-12-30 21:54:29 +0330

    AddFeild_Date_ContactUs_Migration

---
+++ Shop.Databse/Migrations/20201230182356_AddFeild_Date_ContactUs_Migration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Database.Migrations
+{
+    public partial class AddFeild_Date_ContactUs_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "ContactUs",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "ContactUs");
+        }
+    }
+}
--- Shop.Domain/Entities/ContactUs.cs
+++ Shop.Domain/Entities/ContactUs.cs
@@ -4,10 +4,11 @@ using System.Text;
 
 namespace Shop.Domain.Entities
 {
-   public class ContactUs
+    public class ContactUs
     {
         public int Id { get; set; }
         public string Description { get; set; }
         public string UserId { get; set; }
+        public DateTime Date { get; set; }
     }
 }
