ad346d2ebdc17cad21db2436ba1cbe8201047128
mobinhassani9374 <mobinhassani9374@gmail.com>
2021-01-04 14:46:53 +0330

    add payment

--- Shop.Mvc/Controllers/PaymentController.cs
+++ Shop.Mvc/Controllers/PaymentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shop.Services;
+using Shop.Services.Payment.IdPay;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -26,10 +27,10 @@ namespace Shop.Mvc.Controllers
             {
                 var user = _userService.GetUser(UserId);
 
-                var payment = new Services.Payment.IdPay.Payment(_httpClientFactory.CreateClient());
+                var payment = new Payment(_httpClientFactory.CreateClient());
 
-                var obj = new Services.Payment.IdPay.Payment.Request(serviceResult.Data.Id.ToString());
-                obj.amount = decimal.Parse(serviceResult.Data.TotalPrice.ToString());
+                var obj = new Payment.Request(serviceResult.Data.Id.ToString());
+                obj.amount = decimal.Parse($"{serviceResult.Data.TotalPrice.ToString()}0");
                 obj.name = user.FullName;
                 obj.phone = user.PhoneNumber;
                 obj.mail = "";
@@ -45,10 +46,50 @@ namespace Shop.Mvc.Controllers
                 }
                 else
                 {
-                     failRes = ((Services.Payment.IdPay.Payment.RequestRespons_Fail)res).error_message;
+                    failRes = ((Services.Payment.IdPay.Payment.RequestRespons_Fail)res).error_message;
                 }
             }
             return Content(failRes);
+        }
+
+        public async Task<IActionResult> CallBack(int status, int track_id, string id, string order_id)
+        {
+            var serviceResult = new Utility.ServiceResult<string>(true);
+            try
+            {
+                var payment = new Payment(_httpClientFactory.CreateClient());
+                var obj = new Payment.ResultPayment();
+                obj.status = status;
+                obj.track_id = track_id.ToString();
+                obj.id = id;
+                obj.order_id = order_id;
+
+                if (!obj.IsOK)
+                    serviceResult.AddError("پرداختی صورت نگرفت");
+                else
+                {
+                    // تایید تراکنش
+                    var res = await payment.VerifyPayment(obj);
+                    if (res is Payment.PaymentInfo)
+                    {
+                        var tmp = (Payment.PaymentInfo)res;
+                        await _userService.SuccessPayment(int.Parse(order_id));
+                        serviceResult.Data = order_id;
+                    }
+                    else
+                    {
+                        var errorMessage = ((Payment.RequestRespons_Fail)res).error_message;
+                        serviceResult.AddError(errorMessage);
+                    }
+
+                }
+            }
+            catch (System.Exception ex)
+            {
+                serviceResult.AddError(ex.Message);
+            }
+
+            return View(serviceResult);
         }
 
         public async Task<IActionResult> Success(int id)
--- Shop.Services/Payment/IdPay/Payment.cs
+++ Shop.Services/Payment/IdPay/Payment.cs
@@ -54,7 +54,7 @@ namespace Shop.Services.Payment.IdPay
             {
                 get
                 {
-                    return "https://imenforoush.com/callback";
+                    return "https://imenforoush.com/Payment/CallBack";
                 }
             }
 
