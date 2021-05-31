1154f0f2cba57449300d73c7d0f37510983fc23f
mahdihassani9374 <mahdihassani9374@gmail.com>
2021-05-31 10:44:35 +0430

    modify cart and shippingCost = 30000

--- Shop.Mvc/wwwroot/js/app.js
+++ Shop.Mvc/wwwroot/js/app.js
@@ -1,132 +1,138 @@
 var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
         countCart: 0,
         cart: [],
         price: 0,
         loading: false,
         shippingCost: 0,
-        category: []
+        category: [],
     },
     methods: {
-        addCart: function (productId) {
+        addCart: function(productId) {
             var me = this;
-            var l = Ladda.create(document.getElementById('addcart' + productId));
+            var l = Ladda.create(document.getElementById("addcart" + productId));
             $.ajax({
-                url: '/Cart/Add',
-                method: 'post',
-                data: { ProductId: productId }
-            }).then(function (response) {
-                me.countCart = response.data.length;
-                toastr.info(response.message);
-                l.stop();
-            }).fail(function (error) {
-                l.stop()
-                if (error.status === 401) {
-                    Swal.fire({
-                        icon: "error",
-                        title: "هشدار",
-                        text: 'برای افزودن محصول به سبد خریدتان ابتدا باید عضو سایت باشید',
-                        confirmButtonText: 'عضویت در سایت',
-                        footer: '<a href="/Account/Login?returnUrl=addToCart=' + productId + '">اگر قبلا عضو سایت شدید از اینجا وارد شوید</a>'
-                    }).then(function (result) {
-                        if (result.value) {
-                            window.location = '/Account/Register?returnUrl=addToCart=' + productId;
-                        }
-                    })
-                }
-            })
+                    url: "/Cart/Add",
+                    method: "post",
+                    data: { ProductId: productId },
+                })
+                .then(function(response) {
+                    me.countCart = response.data.length;
+                    toastr.info(response.message);
+                    l.stop();
+                })
+                .fail(function(error) {
+                    l.stop();
+                    if (error.status === 401) {
+                        Swal.fire({
+                            icon: "error",
+                            title: "هشدار",
+                            text: "برای افزودن محصول به سبد خریدتان ابتدا باید عضو سایت باشید",
+                            confirmButtonText: "عضویت در سایت",
+                            footer: '<a href="/Account/Login?returnUrl=addToCart=' +
+                                productId +
+                                '">اگر قبلا عضو سایت شدید از اینجا وارد شوید</a>',
+                        }).then(function(result) {
+                            if (result.value) {
+                                window.location =
+                                    "/Account/Register?returnUrl=addToCart=" + productId;
+                            }
+                        });
+                    }
+                });
         },
-        getCart: function () {
+        getCart: function() {
             var me = this;
             me.loading = true;
             $.ajax({
-                url: '/Cart/Get',
-                method: 'post',
-            }).then(function (response) {
-                me.cart = response;
-                me.countCart = response.length;
-                me.computedPrice();
-                me.loading = false;
-            }).fail(function () {
-                me.loading = false;
-            })
+                    url: "/Cart/Get",
+                    method: "post",
+                })
+                .then(function(response) {
+                    me.cart = response;
+                    me.countCart = response.length;
+                    me.computedPrice();
+                    me.loading = false;
+                })
+                .fail(function() {
+                    me.loading = false;
+                });
         },
-        seperator: function (price) {
+        seperator: function(price) {
             var pattern = /(-?\d+)(\d{3})/;
             while (pattern.test(price))
                 price = price.toString().replace(pattern, "$1,$2");
             return price;
         },
-        computedPrice: function () {
+        computedPrice: function() {
             this.price = 0;
             var me = this;
             me.price = 0;
-            me.cart.map(function (item) {
+            me.cart.map(function(item) {
                 if (me.disCount !== 0) {
-                    me.price += (item.price * item.count) * (100 - item.disCount) / 100;
-                }
-                else {
+                    me.price += (item.price * item.count * (100 - item.disCount)) / 100;
+                } else {
                     me.price += item.price * item.count;
                 }
-            })
-            if (me.price < 700000) {
-                me.shippingCost = 20000;
-                me.price += 20000;
-            }
-            else {
-                me.shippingCost = 0;
-            }
+            });
+            // if (me.price < 700000) {
+            //     me.shippingCost = 20000;
+            //     me.price += 20000;
+            // }
+            // else {
+            //     me.shippingCost = 0;
+            // }
+            me.shippingCost = 30000;
+            me.price += 30000;
         },
-        increase: function (productId) {
+        increase: function(productId) {
             var me = this;
             me.loading = true;
             $.ajax({
-                url: '/Cart/Increase',
-                method: 'post',
-                data: { ProductId: productId }
-            }).then(function (response) {               
+                url: "/Cart/Increase",
+                method: "post",
+                data: { ProductId: productId },
+            }).then(function(response) {
                 if (response.isSuccess) {
                     toastr.info(response.message);
                     me.countCart = response.data.length;
                     me.cart = response.data;
-                }
-                else {
+                } else {
                     toastr.error(response.errors[0]);
                 }
                 me.computedPrice();
                 me.loading = false;
             });
         },
-        reduce: function (productId) {
+        reduce: function(productId) {
             var me = this;
             me.loading = true;
             $.ajax({
-                url: '/Cart/Reduce',
-                method: 'post',
-                data: { ProductId: productId }
-            }).then(function (response) {               
+                url: "/Cart/Reduce",
+                method: "post",
+                data: { ProductId: productId },
+            }).then(function(response) {
                 if (response.isSuccess) {
                     toastr.info(response.message);
                     me.countCart = response.data.length;
                     me.cart = response.data;
-                }
-                else {
+                } else {
                     toastr.error(response.errors[0]);
                 }
                 me.computedPrice();
                 me.loading = false;
             });
         },
-        deleteCart: function (productId) {
+        deleteCart: function(productId) {
             var me = this;
-            if (confirm('آیا مطمئن هستید ؟؟ ')) {
+            if (confirm("آیا مطمئن هستید ؟؟ ")) {
                 me.loading = true;
                 $.ajax({
-                    url: '/Cart/Delete',
-                    method: 'post',
-                    data: { ProductId: productId }
-                }).then(function (response) {
+                    url: "/Cart/Delete",
+                    method: "post",
+                    data: { ProductId: productId },
+                }).then(function(response) {
                     me.countCart = response.data.length;
                     me.cart = response.data;
                     toastr.info(response.message);
@@ -135,69 +141,67 @@ var app = new Vue({
                 });
             }
         },
-        getCategory: function () {
+        getCategory: function() {
             var me = this;
             me.loading = true;
             $.ajax({
-                url: '/Category/Get',
-                method: 'post',
-            }).then(function (response) {
+                url: "/Category/Get",
+                method: "post",
+            }).then(function(response) {
                 me.category = response;
-            })
+            });
         },
     },
-    created: function () {
+    created: function() {
         this.getCart();
         this.getCategory();
-    }
-})
-
-Vue.component('item', {
-    template: '#item-template',
-    props: {
-        model: Object
     },
-    data: function () {
+});
+
+Vue.component("item", {
+    template: "#item-template",
+    props: {
+        model: Object,
+    },
+    data: function() {
         return {
             open: false,
-        }
+        };
     },
     computed: {
-        isFolder: function () {
-            return this.model.children &&
-                this.model.children.length
-        }
+        isFolder: function() {
+            return this.model.children && this.model.children.length;
+        },
     },
     methods: {
-        toggle: function () {
+        toggle: function() {
             if (this.isFolder) {
-                this.open = !this.open
+                this.open = !this.open;
             }
-        }
-    }
-})
-
-Vue.component('category', {
-    template: '#category-template',
-    props: {
-        model: Object
+        },
     },
-    data: function () {
+});
+
+Vue.component("category", {
+    template: "#category-template",
+    props: {
+        model: Object,
+    },
+    data: function() {
         return {
             open: false,
-        }
+        };
     },
     computed: {
-        isFolder: function () {
-            return this.model.children &&
-                this.model.children.length
-        }
+        isFolder: function() {
+            return this.model.children && this.model.children.length;
+        },
     },
     methods: {
-        toggle: function () {
+        toggle: function() {
             if (this.isFolder) {
-                this.open = !this.open
+                this.open = !this.open;
             }
-        }
-    }
-})
+        },
+    },
+});
